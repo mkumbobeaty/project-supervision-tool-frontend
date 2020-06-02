37a0e1825d8a09bef886fcb3958434985d6794e5
mkumbobeaty <charsbeaty@gmail.com>
2020-06-02 13:10:50 +0300

    created mocabs for signin

--- src/Auth/components/SignIn/index.js
+++ src/Auth/components/SignIn/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Input, Button, Form } from 'antd';
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import './styles.css';
+import React from "react";
+import PropTypes from "prop-types";
+import { Input, Button, Form, Row, Col } from "antd";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import "./styles.css";
 
 /**
  * @function
@@ -16,54 +16,66 @@ import './styles.css';
  * @since 0.1.0
  */
 const SignIn = ({ loading, history }) => {
-
   return (
     <div className="SignIn">
-      <Form autoComplete="off">
-        {/* username field */}
-        <Form.Item
-          name="username"
-          rules={[
-            {
-              required: true,
-              email: true,
-              message: 'Please input your username!',
-            },
-          ]}
-        >
-          <Input
-            prefix={<UserOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
-            placeholder="Username"
-          />
-        </Form.Item>
-        {/* end username field */}
+      <Row>
+        <Col span={13} offset={1} className="background">
+        </Col>
+        <Col span={10} className="SignInForm" >
+          <div className="Logo">
+            <h1>Tukutane</h1>
+            <span>A platform for matching needs and resources</span>
+            <h5>Please Login to your account</h5>
+          </div>
+          <Form autoComplete="off">
+            {/* username field */}
+            <Form.Item
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  email: true,
+                  message: "Please input your username!",
+                },
+              ]}
+            >
+              <Input
+                prefix={<UserOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
+                placeholder="Username"
+              />
+            </Form.Item>
+            {/* end username field */}
 
-        {/* password field */}
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: 'Please input your Password!' }]}
-        >
-          <Input.Password
-            prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
-            placeholder="Password"
-          />
-        </Form.Item>
-        {/* end password field */}
+            {/* password field */}
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: "Please input your Password!" },
+              ]}
+            >
+              <Input.Password
+                prefix={<LockOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
+                placeholder="Password"
+              />
+            </Form.Item>
+            {/* end password field */}
 
-        {/* submit button */}
-        <Form.Item>
-          <Button
-            type="primary"
-            htmlType="submit"
-            className="signIn-form-button"
-            loading={loading}
-          >
-            Sign In
-          </Button>
-          <div className="version-text">version: 1.0.0</div>
-        </Form.Item>
-        {/* end submit button */}
-      </Form>
+            {/* submit button */}
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="signIn-form-button"
+                loading={loading}
+              >
+                Sign In
+              </Button>
+              <div className="version-text">version: 1.0.0</div>
+            </Form.Item>
+            {/* end submit button */}
+          </Form>
+        </Col>
+      </Row>
     </div>
   );
 };
@@ -75,4 +87,4 @@ SignIn.propTypes = {
   loading: PropTypes.bool.isRequired,
 };
 
-export default SignIn
+export default SignIn;
