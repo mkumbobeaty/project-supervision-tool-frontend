292203337ff7bcc135c71efa92ddafae05e7a129
mloweedgar <mloweedgar0@gmail.com>
2020-08-30 12:17:34 +0300

    feat: make side menu work on map

--- src/App/Map/components/SideMenu/SideMenuItem/index.js
+++ src/App/Map/components/SideMenu/SideMenuItem/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './styles.css';
 
-const SideMenuItem = ({title, bgImage}) => {
+const styles = {
+    background: 'white',
+    borderBottom: '1px solid blue',
+}
+
+const SideMenuItem = ({title, bgImage, setActive, ItemKey, active}) => {
+    console.log(active === ItemKey);
     return (
-        <div className="SideMenuItem">
+        <div
+            className="SideMenuItem"
+            onClick={() => setActive(ItemKey)}
+            style={active === ItemKey ? styles : {}}
+        >
             <img src={bgImage} width={80}/>
-            <div className="side-menu-item-title">{title}</div>
+            <div className="side-menu-item-title" style={active === ItemKey ? {color: "#2d98d4"} : {color: "white"}}>{title}</div>
         </div>
     );
 }
--- src/App/Map/components/SideMenu/index.js
+++ src/App/Map/components/SideMenu/index.js
@@ -1,15 +1,31 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import './styles.css';
-import initiativeImg from '../../../../assets/icons/white-initiative.svg'
-import humanResourceImg from '../../../../assets/icons/white-human-resources.svg'
+import whitIinitiativeImg from '../../../../assets/icons/white-initiative.svg'
+import initiativeImg from '../../../../assets/icons/initiatives-zero-opacity.svg'
+import humanResourceImg from '../../../../assets/icons/human-resources-zero-opacity.svg'
+import whiteHhumanResourceImg from '../../../../assets/icons/white-human-resources.svg'
+
 
 import SideMenuItem from "./SideMenuItem";
  const SideMenu = () => {
+     const [active, setActive] = useState('initiative');
  return (
      <div className="SideMenu">
-         <SideMenuItem bgImage={initiativeImg} title="Initiative"/>
-         <SideMenuItem bgImage={humanResourceImg} title="Human Resource"/>
+         <SideMenuItem
+             bgImage={active === "initiative" ? initiativeImg : whitIinitiativeImg}
+             title="Initiative"
+             setActive={setActive}
+             active={active}
+             ItemKey="initiative"
+         />
+         <SideMenuItem
+             bgImage={active === "human-resource" ? humanResourceImg : whitIinitiativeImg}
+             title="Human Resources"
+             setActive={setActive}
+             active={active}
+             ItemKey="human-resource"
+         />
      </div>
  )
  }
