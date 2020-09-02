18d8ab5ac720179940c17b92875cec5e84980ed8
mloweedgar <mloweedgar0@gmail.com>
2020-09-02 11:11:48 +0300

    feat: use  api in https

--- src/API/index.js
+++ src/API/index.js
@@ -2,7 +2,7 @@
 import Axios from "axios";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "http://pamoja-backend.herokuapp.com";
+  process.env.REACT_APP_API_URL || "https://pamoja-backend.herokuapp.com";
 Axios.defaults.baseURL = `${API_BASE_URL}/api/v1`;
 
 /**
