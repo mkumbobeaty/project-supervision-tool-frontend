5134d4d5d0ca3cf17d4ce88f8bc9814138d033b0
mloweedgar <mloweedgar0@gmail.com>
2020-11-19 17:20:27 +0300

    feat: display project status details on nav detail page

--- src/App/Map/components/SideNav/components/NavItemDetails/index.js
+++ src/App/Map/components/SideNav/components/NavItemDetails/index.js
@@ -1,43 +1,50 @@
 import React from 'react';
-import { Button } from 'antd';
+import {Button} from 'antd';
 import './styles.css'
 import CustomSearch from "./components/CustomSearch";
+import SummarySection from "./components/SummarySection";
 
 const styles = {width: '16vw'}
 
-function NavItemDetails({ activeItem }) {
+function NavItemDetails({activeItem}) {
+    const projectStatusData = [
+        {name: 'Active', count: 120},
+        {name: 'Closed', count: 30},
+        {name: 'Dropped', count: 4},
+        {name: 'Pipeline', count: 6},
+        ]
 
-    return(
+    return (
         <div
-            style={activeItem === '' ? { display: 'none'} : styles }
+            style={activeItem === '' ? {display: 'none'} : styles}
             className='NavItemDetails'
         >
             <section className='overview'>
-                <CustomSearch />
+                <CustomSearch/>
                 <section className='overview-details'>
                     <div className='overview-title'>Projects Overview</div>
                     <Button type="primary" style={{fontSize: 10}} size='small'>FILTERS</Button>
                 </section>
                 <section className='overview-table'>
-                        <div className='overview-table-item'>
-                            <div title='Total projects count'>Projects</div>
-                            <div>2,859</div>
-                        </div>
-                        <div className='overview-table-item'>
-                            <div title='Total Commitment Amount'>Com.Amt</div>
-                            <div>$295.74b</div>
-                        </div>
-                        <div className='overview-table-item'>
-                            <div title='Projects Locations at regional level'>Regions</div>
-                            <div>18,238</div>
-                        </div>
-                        <div className='overview-table-item'>
-                            <div title='Projects locations at district level'>Districts</div>
-                            <div>134</div>
-                        </div>
+                    <div className='overview-table-item'>
+                        <div title='Total projects count'>Projects</div>
+                        <div>2,859</div>
+                    </div>
+                    <div className='overview-table-item'>
+                        <div title='Total Commitment Amount'>Com.Amt</div>
+                        <div>$295.74b</div>
+                    </div>
+                    <div className='overview-table-item'>
+                        <div title='Projects Locations at regional level'>Regions</div>
+                        <div>18,238</div>
+                    </div>
+                    <div className='overview-table-item'>
+                        <div title='Projects locations at district level'>Districts</div>
+                        <div>134</div>
+                    </div>
                 </section>
+                <SummarySection sectionName='Project status' items={projectStatusData}/>
             </section>
-
         </div>
     );
 }
