fab84ef9d0e86ed3ad4cb8b2ef776140b0999814
mloweedgar <mloweedgar0@gmail.com>
2020-11-19 10:35:07 +0300

    feat(navitemdetail): add search ui

--- src/App/Map/components/SideNav/components/NavItemDetails/index.js
+++ src/App/Map/components/SideNav/components/NavItemDetails/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Input, AutoComplete } from 'antd';
 import './styles.css'
+import CustomSearch from "./components/CustomSearch";
 
 const styles = {width: '16vw'}
 
 function NavItemDetails({ activeItem }) {
 
     return(
-        <div style={activeItem === '' ? { display: 'none'} : styles } className='NavItemDetails'>NavItemDetails</div>
+        <div
+            style={activeItem === '' ? { display: 'none'} : styles }
+            className='NavItemDetails'
+        >
+            <CustomSearch />
+
+        </div>
     );
 }
 
