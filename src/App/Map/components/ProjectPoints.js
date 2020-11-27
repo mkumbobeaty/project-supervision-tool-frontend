82cc138a82841aa8c80d04561f598f38984f7e07
mloweedgar <mloweedgar0@gmail.com>
2020-11-27 22:37:50 +0300

    feat: display project as markers

--- src/App/Map/components/ProjectPoints.js
+++ src/App/Map/components/ProjectPoints.js
@@ -1,45 +1,88 @@
-import { withLeaflet, GeoJSON } from "react-leaflet";
+import {withLeaflet, GeoJSON, Tooltip, Popup, Marker} from "react-leaflet";
 import * as turf from '@turf/turf'
 import React, {Component} from "react";
 import PropTypes from 'prop-types';
+import Spiderfy from "./Spiderfy";
+import {Link} from "react-router-dom";
 
-class ProjectPoints extends Component{
+class ProjectPoints extends Component {
 
     static propTypes = {
         data: PropTypes.object.isRequired
     }
 
-    generateProjectMarkers = () => {
+    handleSpiderfyClick = marker => {
+        console.log(marker);
+    };
+
+    handleSpiderfy = markers => {
+        console.log(markers);
+    };
+
+    handleUnspiderfy = markers => {
+        console.log(markers);
+    };
+
+    generateProjectMarkers = (regionProjects) => {
         const {regionDetails} = this.props;
-        if (regionDetails.geom) {
-            console.log('look at geom', regionDetails.geom);
-            const feature = {
-                "type": "Feature",
-                "geometry": regionDetails?.geom,
-            }
-            const point1 = turf.pointOnFeature(feature);
-            const point2 = turf.pointOnFeature(feature);
-            console.log('point1', point1);
-            console.log('point2', point2);
-        }
+        const regionId = regionDetails.id;
+        const points = regionProjects.map(regionProject => {
+            const {locations} = regionProject;
+            return locations.map(location => {
+
+                const pointOnDistrict = turf.pointOnFeature({
+                    "type": "Feature",
+                    "geometry": location?.district.geom,
+                });
+
+                const key = `${regionProject.id}-${location?.district.id}`;
+
+                return {
+                    ...pointOnDistrict, "properties": {
+                        "name": regionProject.name,
+                        "description": regionProject.description,
+                        "districtId": location?.district.id,
+                        "key": key,
+                        "regionId": location?.district.region_id,
+                        "districtName": location?.district.name,
+                        "projectId": regionProject.id,
+                    }
+                };
+            });
+        });
+
+        const flattenedArrayOfPoint = points.flat();
+
+        return flattenedArrayOfPoint.filter(p => p.properties.regionId === regionId);
     }
-    onEachFeature = (feature, layer) => {
-        const { map } = this.props.leaflet;
-        console.log('on each feature  inside ProjectPoints')
-    }
+
+
 
     render() {
-        const data = this.generateProjectMarkers();
-        const geoJsonObject= {
-            "type": "Feature",
-            "geometry": data?.geom,
-        }
-
-        return data?.geom ? <GeoJSON
-            data={geoJsonObject}
-            style={{ "opacity": 0.5, "fillOpacity": 0.05, "weight": 1 }}
-            onEachFeature={this.onEachFeature}
-        /> : '';
+        const {regionProjects} = this.props;
+        const data = regionProjects.length > 0 ? this.generateProjectMarkers(regionProjects) : '';
+        return data.length > 0 ? (
+            <Spiderfy
+                onClick={this.handleSpiderfyClick}
+                onSpiderfy={this.handleSpiderfy}
+                onUnspiderfy={this.handleUnspiderfy}
+            >
+                { data.map( ({ geometry, properties }) => {
+                    return (
+                        <Marker
+                            position={geometry.coordinates.reverse()}
+                            title={properties.districtName}
+                            key={properties.key}
+                        >
+                            <Popup>
+                                <div><b>District name:</b> { properties.districtName}</div>
+                                <div><b>Project name:</b> { properties.name}</div>
+                                <Link to="/app/map">View project</Link>
+                            </Popup>
+                        </Marker>
+                    );
+                })}
+            </Spiderfy>) : '';
     }
 }
 
