463f7465116f01ddf98418f5684ba0b3fbe9f1af
mloweedgar <mloweedgar0@gmail.com>
2021-01-05 23:37:35 +0300

    feat(subprojectlocations): display subproject locations on map

--- src/App/Map/components/SubProjectLocations.js
+++ src/App/Map/components/SubProjectLocations.js
@@ -1,33 +1,71 @@
-
 import React, {Component} from 'react';
-import {GeoJSON, Popup, withLeaflet} from "react-leaflet";
+import {GeoJSON, Marker, Popup, withLeaflet} from "react-leaflet";
 import L from "leaflet";
+import Spiderfy from "./Spiderfy";
 
-class SubProjectLocations extends Component{
+/**
+ * @function
+ * @name mapSubProjectElementsToLocationPoints
+ * @description generate points from su project elements
+ */
+const mapSubProjectElementsToLocationPoints = subProjectElements => {
+    const pointsSubArray = subProjectElements.map(
+        ({locations, name}) => locations.map(
+            ({point, id}) => ({coordinates: point.coordinates, id, name})));
+    return pointsSubArray.flat();
+}
+
+/**
+ * @class
+ * @name SubProjectLocations
+ * @description component that renders sub project locations as polygons on map with
+ *  sub project elements as marker points on top of those polygons
+ */
+class SubProjectLocations extends Component {
 
     componentDidUpdate(prevProps, prevState, snapshot) {
-        if (((prevProps.subProject !== this.props.subProject) && this.props.subProject)){
-            const { map } = this.props.leaflet;
+        if (((prevProps.subProject !== this.props.subProject) && this.props.subProject)) {
+            const {map} = this.props.leaflet;
             map.setView(L.latLng(-6.161184, 35.745426), 6);
         }
     }
 
-    renderDistricts = (subProject) => subProject?.sub_project_locations.map(({ district, id }) => {
+
+    renderSubProjectElements = (subProjectElements) => {
+        const data = mapSubProjectElementsToLocationPoints(subProjectElements);
+        debugger;
+        return data.map(({coordinates, id, name }) => (
+                <Marker position={[coordinates[1], coordinates[0]]}>
+                    <Popup>
+                        <div> { name }</div>
+                    </Popup>
+                </Marker>
+        ));
+    }
+
+    renderDistricts = (subProject) => subProject?.sub_project_locations.map(({district, id}) => {
 
         return (
             <GeoJSON key={id} data={district.geom}>
                 <Popup>
-                    <div><b>SubProject :</b> { subProject.name}</div>
-                    <div><b>District :</b> { district.name}</div>
+                    <div><b>SubProject :</b> {subProject.name}</div>
+                    <div><b>District :</b> {district.name}</div>
                 </Popup>
             </GeoJSON>
         );
     })
 
-    render () {
-        const { subProject } = this.props;
+    render() {
+        const {subProject} = this.props;
 
-        return subProject ? (<>{this.renderDistricts(subProject)}</>) : '';
+        return subProject ? (
+            <>
+                {this.renderDistricts(subProject)}
+                <Spiderfy>
+                    {this.renderSubProjectElements(subProject.sub_project_items)}
+                </Spiderfy>
+                </>
+        ) : '';
     }
 }
 
