af93beb89219f6f4930a8937c6cbd76492989e3c
mloweedgar <mloweedgar0@gmail.com>
2021-02-18 12:53:01 +0300

    feat: move important redux files to redux folder

---
+++ src/redux/rootEpic.js
@@ -0,0 +1,22 @@
+import { combineEpics } from "redux-observable";
+import { mapRootEpic } from "./modules/map/epics";
+import { projectsRootEpic } from "./modules/projects/epics";
+import { restoreAccessTokenEpic } from './modules/app/epics';
+import { loginEpic } from "./modules/auth/epics";
+import { focalPeopleEpic } from "../App/FocalPeople/duck/epics";
+import { sectorsEpic } from '../App/ProjectsList/Projects/components/ProjectsSectors/duck/epics';
+import { projectDetailsEpic } from '../App/ProjectsList/Projects/components/ProjectsDetails/duck/epics';
+import {subProjectsEpic} from './modules/subProjects/epics';
+
+export const rootEpic = combineEpics(
+    loginEpic,
+    restoreAccessTokenEpic,
+    mapRootEpic,
+    focalPeopleEpic,
+    restoreAccessTokenEpic,
+    mapRootEpic,
+    projectsRootEpic,
+    sectorsEpic,
+    projectDetailsEpic,
+    subProjectsEpic
+)
