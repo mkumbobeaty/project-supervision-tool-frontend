8d0c98b7c48b21b8a689be51773fa3df668b1518
mkumbobeaty <charsbeaty@gmail.com>
2021-02-23 15:39:04 -0500

    feat(sub-projects): implemented map that display selected sub-project

--- src/App/ProjectsList/Sub-projects/components/SubProjectsDetails/index.js
+++ src/App/ProjectsList/Sub-projects/components/SubProjectsDetails/index.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
 import { Col, Layout, Row, Spin } from 'antd';
-import ItemsChat from "./Charts";
-import ProjectSubProjects from "./SubProjectMilestone";
 import SidebarSection from "./SideBar";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { projectOperation, projectSelectors } from "../../../../../redux/modules/projects";
-import ProgressChart from "./ProgressChart";
 import SubProjectEquipment from "./SubProjectEquipment";
 
 import "./styles.css";
 import SubProjectsMilestone from "./SubProjectMilestone";
 import SubProjectHumanResource from "./SubProjectHumanResource";
 import BaseMap from "../../../../Map/BaseMap";
+import MapDashboard from "../../../../Map/"
+import SubProjectLocations from "../../../../Map/components/SubProjectLocations";
+import { mapActions, mapSelectors } from "../../../../../redux/modules/map";
 
 const { Content, Sider } = Layout;
 
@@ -25,12 +25,12 @@ class SubProject extends Component {
   }
 
   render() {
-    const { sub_project, loading } = this.props;
+    const { sub_project, loading, mapLoading, getWfsLayerData } = this.props;
     return (
       <Layout className="sub-project-layout">
         <Spin spinning={loading} tip="Loading..." >
           <Content style={{ padding: '0 50px' }}>
-            <h3>{sub_project?.name}</h3>
+            <h3 id="sub_project_name">{sub_project?.name}</h3>
             <Layout className="sub-project-inner-layout" >
               <Sider className="sider" width={350}>
                 <div className="sidebar-header">
@@ -47,16 +47,20 @@ class SubProject extends Component {
               <Content className="sub-project-contents">
                 <Row>
                   <Col span={11} className="sub_project_map"  >
-                    <BaseMap />
+                    <Spin spinning={mapLoading} tip="Loading data...">
+                      <BaseMap ref={this.map} zoomControl={false}>
+                        <SubProjectLocations getWfsLayerData={getWfsLayerData} subProject={sub_project} />
+                      </BaseMap>
+                    </Spin>
                   </Col>
                   <Col span={12} >
-                  <SubProjectsMilestone sub_project={sub_project} offset={1} />
+                    <SubProjectsMilestone sub_project={sub_project} offset={1} />
                   </Col>
                   <Col span={11} style={{ marginTop: 26 }}>
                     <SubProjectHumanResource sub_project={sub_project} />
                   </Col>
-                  <Col span={12}  style={{ marginTop: 26 }}>
-                    < SubProjectEquipment sub_project={sub_project}  offset={1}/>
+                  <Col span={12} style={{ marginTop: 26 }}>
+                    < SubProjectEquipment sub_project={sub_project} offset={1} />
                   </Col>
                 </Row>
               </Content>
@@ -70,12 +74,16 @@ class SubProject extends Component {
 const mapStateToProps = (state) => {
   return {
     sub_project: projectSelectors.getSubProjectSelector(state),
-    loading: projectSelectors.getSubProjectLoadingSelector(state)
+    loading: projectSelectors.getSubProjectLoadingSelector(state),
+    mapLoading: mapSelectors.getMapLoadingSelector(state),
+
   };
 };
 
 const mapDispatchToProps = {
   getSubProject: projectOperation.getSubProjectStart,
+  getWfsLayerData: mapActions.getWfsLayerDataStart,
+
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(SubProject);
