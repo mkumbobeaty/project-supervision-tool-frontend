eb34b30e059f40250057d9a18c1f7a5e631015b9
mkumbobeaty <charsbeaty@gmail.com>
2021-04-13 15:35:30 -0400

    refactor(home): change icons

--- src/App/navigation/Home.js
+++ src/App/navigation/Home.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import projectIcon from '../../assets/icons/projects.svg';
-import adminPanelIcons from '../../assets/icons/admin-panel.svg';
+import adminPanelIcons from '../../assets/icons/admin-panel-grey.svg';
 import NavigationMenu from '../components/NavigationMenu';
 import subProjectsIcon from '../../assets/icons/sub-projects.svg';
 import mapsIcon from "../../assets/icons/maps.svg";
-import contractsIcons from '../../assets/icons/contract-management.svg';
-import documentIcons from '../../assets/icons/shared-documents.svg';
+import contractsIcons from '../../assets/icons/contract-management-grey.svg';
+import documentIcons from '../../assets/icons/shared-documents-grey.svg';
 import modules from '../../modules.json';
+
+// import adminPanelIcons from '../../assets/icons/admin-pane-greyl.svg';
 
 /* constants */
 const routes = [
@@ -35,18 +37,24 @@ const routes = [
     path: '/admin-panel',
     icon: adminPanelIcons,
     description: modules.adimin_panel,
+    disabled: true,
+
   },
   {
     name: 'CONTRACT MANAGEMENT',
     path: '/contracts',
     icon: contractsIcons,
     description: modules.contract_management,
+    disabled: true,
+
   },
   {
     name: 'Documents',
     path: '/documents',
     icon: documentIcons,
     description: modules.documents,
+    disabled: true,
+
   }
 ];
 
