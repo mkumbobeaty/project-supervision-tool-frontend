187cc7b6f3cb48669ea94b3a5563043799a96605
mkumbobeaty <charsbeaty@gmail.com>
2021-04-28 16:29:11 -0400

    feat(map): added filter for sub-components

--- src/App/Map/components/SideNav/components/NavItemDetails/components/ProjectsMenuItemDetails/components/Component&SubComponentFilter /index.js
+++ src/App/Map/components/SideNav/components/NavItemDetails/components/ProjectsMenuItemDetails/components/Component&SubComponentFilter /index.js
@@ -11,28 +11,13 @@ const prepareFilterItems = (items) => items.map(({ name, id, sub_components }) =
 
 
 const ComponentSubComponentFilter = ({ setComponentsSubComponentFilter, project }) => {
-    const [componentIds, setComponentsId] = useState([]);
-    
-    useEffect(() => {
-        setComponentsSubComponentFilter(componentIds.join(','));
-    }, [componentIds]);
-
-    const handleOnclickFilterItem = (package_id) => {
-        if (componentIds.includes(package_id)) {
-            const filterUncheckedItem = componentIds.filter((i) => i !== package_id);
-            setComponentsId(filterUncheckedItem);
-        }
-        else {
-            setComponentsId([...componentIds, package_id]);
-        }
-
-    }
-
+  
 const itemsData = project.components.length > 0 ? prepareFilterItems(project.components) : [];
 
     return (
         <MultlevelFilter
             items={itemsData}
+            handleFilter={setComponentsSubComponentFilter}
         />
     )
 }
