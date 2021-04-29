3d702739802878da98119b1340f5f1b3a4fab854
mkumbobeaty <charsbeaty@gmail.com>
2021-04-29 16:33:02 -0400

    feat(map): improve markers styles and data

--- src/App/Map/components/ProjectPoints.js
+++ src/App/Map/components/ProjectPoints.js
@@ -6,8 +6,9 @@ import Spiderfy from "./Spiderfy";
 import * as turf from '@turf/turf';
 import MapPopupDetail from "./PopupDetails";
 import randomColor from 'randomcolor';
+import { invertColor, moneyFormatWithApproximation } from "../../../Util";
 
-    
+
 class ProjectPoints extends Component {
 
     static propTypes = {
@@ -22,36 +23,59 @@ class ProjectPoints extends Component {
         const { getProject } = this.props;
         getProject(project_id);
     };
-    
+
+    getMarkerDiameter = (amount, maxAmount, maxDiameter = 80, minDiameter = 40) => {
+        const diameter = amount * maxDiameter / maxAmount;
+        if (diameter > minDiameter ) return diameter;
+        return minDiameter;
+    }
+
+    getMaxAmount = (projects) => {
+        const commitment_amounts = projects.map(({ details: { commitment_amount } }) => commitment_amount.amount);
+        return Math.max(...commitment_amounts);
+    }
 
     render() {
 
         const { projects, project, loading } = this.props;
-        const size =  [
-            {width: '80', height: '80', id: "P123134"},
-            {width: '40', height: '40', id: "P171189"},
-            {width: '20', height: '20', id: "P165128"}
-    ]
 
         return (
             <Spiderfy
             >
-                { projects.map(({ regions, id }) => {
+                { projects.map(({ regions, id, details }) => {
+
+                    const { commitment_amount } = details;
+                    const { amount } = commitment_amount;
+                    const commitment_money = moneyFormatWithApproximation(amount)
+                    // const total_money = moneyFormat(total_project_cost.amount)
+                    const maxAmount = this.getMaxAmount(projects);
+                    console.log("maximum value", maxAmount)
+                    const dimension = this.getMarkerDiameter(amount, maxAmount);
+
                     return regions.length > 0 ? regions.map((region) => {
                         const polygon = JSON.parse(region.geom);
                         const { geometry } = turf.pointOnFeature(polygon);
-                        console.log(projects)
-                        
+                        const color = randomColor();
+                        const invertedColor = invertColor(color);
+
                         const customizedIcon = divIcon({
                             className: 'customizedIcon',
                             html: `<div 
-                            style='background-color:${randomColor()}; 
-                            width: ${size.width}px ;
-                            height: ${size.height}px;' 
+                            style='background-color:${color}; 
+                            width: ${dimension}px ;
+                            height: ${dimension}px;' 
                             class='marker-pin'>
-                            </div><h4> ${projects.length}</h4>`,
-                            iconSize: [30, 42],
-                            iconAnchor: [15, 42]
+                            </div>
+                            <h4 style='color: ${invertedColor};
+                            top: ${dimension / 2}px;
+                            left: ${dimension / 2}px;
+                            font-size: ${dimension/4}px
+                            '
+                            > 
+                            ${commitment_money}
+                            </h4>`,
+                            iconSize: [dimension, 42],
+                            iconAnchor: [0, 0]
                         });
 
                         return (
--- src/Util/index.js
+++ src/Util/index.js
@@ -212,6 +212,30 @@ export const moneyFormat = (labelValue) => {
 
 }
 
+/**
+ * @function
+ * @name moneyFormatWithApproximation
+ * @description generates number range based on a number
+ * @param {Number} labelValue
+ * @returns {String} rounded up number
+ */
+export const moneyFormatWithApproximation = (labelValue) => {
+    // Nine Zeroes for Billions
+    return Math.abs(Number(labelValue)) >= 1.0e+9
+
+        ? Number.parseFloat(Math.abs(Number(labelValue)) / 1.0e+9).toPrecision(3) + "B" 
+        // Six Zeroes for Millions
+        : Math.abs(Number(labelValue)) >= 1.0e+6
+
+            ? Number.parseFloat(Math.abs(Number(labelValue)) / 1.0e+6).toPrecision(3) + "M"
+            // Three Zeroes for Thousands
+            : Math.abs(Number(labelValue)) >= 1.0e+3
+
+                ? Number.parseFloat(Math.abs(Number(labelValue)) / 1.0e+3).toPrecision(3) + "K"
+
+                : Number.parseFloat(Math.abs(Number(labelValue))).toPrecision(3) ;
+
+}
 
 /**
  * @function
@@ -315,4 +339,39 @@ export const stringToGeoJson = (str,spatialType) => {
         }
     }
 
+}
+
+
+export function invertColor(hex, bw=true) {
+    if (hex.indexOf('#') === 0) {
+        hex = hex.slice(1);
+    }
+    // convert 3-digit hex to 6-digits.
+    if (hex.length === 3) {
+        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    if (hex.length !== 6) {
+        throw new Error('Invalid HEX color.');
+    }
+    var r = parseInt(hex.slice(0, 2), 16),
+        g = parseInt(hex.slice(2, 4), 16),
+        b = parseInt(hex.slice(4, 6), 16);
+    if (bw) {
+        // http://stackoverflow.com/a/3943023/112731
+        return (r * 0.299 + g * 0.587 + b * 0.114) > 186
+            ? '#000000'
+            : '#FFFFFF';
+    }
+    // invert color components
+    r = (255 - r).toString(16);
+    g = (255 - g).toString(16);
+    b = (255 - b).toString(16);
+    // pad each with zeros and return
+    return "#" + padZero(r) + padZero(g) + padZero(b);
+}
+
+function padZero(str, len) {
+    len = len || 2;
+    var zeros = new Array(len).join('0');
+    return (zeros + str).slice(-len);
 }
