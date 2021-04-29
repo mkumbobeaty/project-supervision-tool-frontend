5c7ca2176770f415a00c7921507e334bce44a82b
mkumbobeaty <charsbeaty@gmail.com>
2021-04-29 00:05:55 -0400

    feat(map): improve markers on a map

--- src/App/Map/components/ProjectPoints.js
+++ src/App/Map/components/ProjectPoints.js
@@ -5,48 +5,54 @@ import PropTypes from 'prop-types';
 import Spiderfy from "./Spiderfy";
 import * as turf from '@turf/turf';
 import MapPopupDetail from "./PopupDetails";
+import randomColor from 'randomcolor';
 
+    
 class ProjectPoints extends Component {
 
     static propTypes = {
         projects: PropTypes.array.isRequired,
-        projects: PropTypes.object.isRequired,
-
+        project: PropTypes.object.isRequired,
+        loading: PropTypes.bool.isRequired,
+        getProject: PropTypes.func,
     }
 
-    handleSpiderfyClick = marker => {
-        console.log(marker);
-    };
-
-    handleSpiderfy = markers => {
-        console.log(markers);
-    };
-
-    handleUnspiderfy = markers => {
-        console.log(markers);
-    };
 
     handleProjectPopup = (project_id) => {
         const { getProject } = this.props;
         getProject(project_id);
     };
+    
 
     render() {
 
         const { projects, project, loading } = this.props;
+        const size =  [
+            {width: '80', height: '80', id: "P123134"},
+            {width: '40', height: '40', id: "P171189"},
+            {width: '20', height: '20', id: "P165128"}
+    ]
 
         return (
             <Spiderfy
-                onClick={this.handleSpiderfyClick}
-                onSpiderfy={this.handleSpiderfy}
-                onUnspiderfy={this.handleUnspiderfy}
             >
                 { projects.map(({ regions, id }) => {
                     return regions.length > 0 ? regions.map((region) => {
                         const polygon = JSON.parse(region.geom);
                         const { geometry } = turf.pointOnFeature(polygon);
-
-                        const customizedIcon = divIcon({ className: 'customizedIcon' });
+                        console.log(projects)
+                        
+                        const customizedIcon = divIcon({
+                            className: 'customizedIcon',
+                            html: `<div 
+                            style='background-color:${randomColor()}; 
+                            width: ${size.width}px ;
+                            height: ${size.height}px;' 
+                            class='marker-pin'>
+                            </div><h4> ${projects.length}</h4>`,
+                            iconSize: [30, 42],
+                            iconAnchor: [15, 42]
+                        });
 
                         return (
                             <Marker
@@ -57,7 +63,7 @@ class ProjectPoints extends Component {
                                 onClick={() => this.handleProjectPopup(id)}
                             >
                                 <Popup>
-                                    <MapPopupDetail project={project} loading={loading} />  
+                                    <MapPopupDetail project={project} loading={loading} />
                                 </Popup>
                             </Marker>
                         );
