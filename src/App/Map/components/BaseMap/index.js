e970b5e1d429f51a590ede6447e2ad2c379d63fe
mloweedgar <mloweedgar0@gmail.com>
2021-07-22 22:18:33 +0300

    fix: show default tile layer in basemap switcher

--- src/App/Map/components/BaseMap/index.js
+++ src/App/Map/components/BaseMap/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from "prop-types";
-import {MapContainer, ZoomControl } from 'react-leaflet';
+import {MapContainer} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-fullscreen/dist/Leaflet.fullscreen.min';
 import 'leaflet-fullscreen/dist/leaflet.fullscreen.css';
@@ -8,9 +8,9 @@ import 'leaflet-basemaps/L.Control.Basemaps-min';
 import 'leaflet-basemaps/L.Control.Basemaps.css';
 import L from 'leaflet';
 import "./styles.css";
-import BaseMapSwitcher from "./components/BaseMapSwitcher";
 import LayerControl from "../LayerControl";
-import ControlBar from "./components/ControlBar";
+import './L.Control.ZoomBar';
+import './L.Control.ZoomBar.css';
 
 delete L.Icon.Default.prototype._getIconUrl;
 
@@ -32,11 +32,38 @@ const state = {
 
 const BaseMap = ({children, position}) => {
 
+    const BASE_MAPS = [
+
+        L.tileLayer('//tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+            label: 'OpenStreetMap Black and White'  // optional label used for tooltip
+        }),
+        L.tileLayer('//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+            label: 'OpenStreetMap Standard'  // optional label used for tooltip
+        }),
+        L.tileLayer('//mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+            label: 'Google Satellite'
+        })
+    ];
+
+    const whenCreated = (map) => {
+        // add basemaps switcher to map
+        map.addControl(L.control.basemaps({
+            basemaps: BASE_MAPS,
+            position: "bottomleft",
+            tileX: 0,  // tile X coordinate
+            tileY: 0,  // tile Y coordinate
+            tileZ: 1 ,  // tile zoom level,
+        }));
+
+        // add zoom control to map
+        new L.Control.ZoomBar({position: 'topright'}).addTo(map);
+    }
+
     return (
-        <MapContainer center={position} fullscreenControl={{ position: 'topright'}} zoom={state.zoom} className="base-map" zoomControl={false} >
+        <MapContainer center={position} whenCreated={whenCreated} fullscreenControl={{ position: 'topright'}} zoom={state.zoom} className="base-map" zoomControl={false} >
             <LayerControl />
-            <BaseMapSwitcher />
-            <ControlBar />
             {children}
         </MapContainer>
     )
