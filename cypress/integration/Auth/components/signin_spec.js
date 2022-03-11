6ca9a4ec81cf8bf84d245cf690a045a1de191e03
mloweedgar <mloweedgar0@gmail.com>
2022-03-11 13:56:11 +0300

    test(sign in): should not  signin with invalid credentials

--- cypress/integration/Auth/components/signin_spec.js
+++ cypress/integration/Auth/components/signin_spec.js
@@ -9,4 +9,15 @@ describe('Signin', () => {
         cy.get('button[type=submit]').should('exist').should('contain', 'Log In').click();
         cy.url().should('include', '/#!/projects');
     });
+
+    it('should not signin with invalid credentials', () => {
+        cy.visit('http://localhost:3000/#!/signin');
+        cy.get('.Logo h2').should('contain', 'Projects Supervison tool');
+        cy.get('.Logo h5').should('contain', 'Please Login to your account');
+
+        cy.get('#email').type('testing@project-supervision-tool.com');
+        cy.get('#password').type('Pass@Too');
+        cy.get('button[type=submit]').should('exist').should('contain', 'Log In').click();
+        cy.get('.Logo + div').should('contain', 'Request failed with status code 401');
+    });
 });
