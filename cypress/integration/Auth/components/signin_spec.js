9b1b922a488d6e8312497a0feccccb693fcbb2b9
mloweedgar <mloweedgar0@gmail.com>
2022-03-11 13:31:48 +0300

    test(sign in): sign in with valid credentials

---
+++ cypress/integration/Auth/components/signin_spec.js
@@ -0,0 +1,12 @@
+describe('Signin', () => {
+    it('should signin with valid credentials', () => {
+        cy.visit('http://localhost:3000/#!/signin');
+        cy.get('.Logo h2').should('contain', 'Projects Supervison tool');
+        cy.get('.Logo h5').should('contain', 'Please Login to your account');
+
+        cy.get('#email').type('testing@project-supervision-tool.com');
+        cy.get('#password').type('Pass@Tool');
+        cy.get('button[type=submit]').should('exist').should('contain', 'Log In').click();
+        cy.url().should('include', '/#!/projects');
+    });
+});
