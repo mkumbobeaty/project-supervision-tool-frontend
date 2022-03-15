ccf4f4cd5018703eb190a1d10ceee17c5a2beb8e
mkumbobeaty <charsbeaty@gmail.com>
2022-03-15 13:43:56 +0300

    test(project): render breadcrumb and user icon

--- cypress/integration/Projects/components/project_list_spec.js
+++ cypress/integration/Projects/components/project_list_spec.js
@@ -1,14 +1,24 @@
 describe('Projects', () => {
-    
+
     beforeEach(() => {
         cy.visit('http://localhost:3000/#!/projects')
-    });
-
-    it('should bulk actions on projects page', () => {
         cy.get('#email').type('testing@project-supervision-tool.com');
         cy.get('#password').type('Pass@Tool');
         cy.get('button[type=submit]').should('exist').should('contain', 'Log In').click();
-        cy.get('.Breadcrumb a').should('contain', 'Projects');
+    });
 
-    })
+    it('should render the top navigation bar', () => {
+        cy.get('.Breadcrumb a').should('contain', 'Projects');
+        cy.get('.UserMenu').should('be.visible').as('userIcon')
+        cy.get('@userIcon').trigger('mouseover').as('triggleButton')
+        cy.get('.UserProfileMenu > li').should(($lis) => {
+            expect($lis).to.have.length(2)
+            expect($lis.eq(0)).to.contain('Change Password')
+            expect($lis.eq(1)).to.contain('Sign Out')
+        })
+        cy.get('@userIcon').trigger('mouseout');
+        cy.get('.UserProfileMenu > li').should('not.be.visible');
+
+    });
+
 })
