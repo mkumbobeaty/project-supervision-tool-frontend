5d0d71fdaffb25a99c9f57fe2ac8e7a5e7e40bb9
edgar <mloweedgar0@gmail.com>
2022-04-03 19:05:30 +0300

    fix(map): display sub projects well

--- src/App/Map/components/SubProjectPoints.js
+++ src/App/Map/components/SubProjectPoints.js
@@ -1,12 +1,10 @@
-import {Marker, useMapEvents, GeoJSON, Popup} from "react-leaflet";
-import React, {useState} from "react";
+import { Marker, useMapEvents, GeoJSON, Popup } from "react-leaflet";
+import React, { useState } from "react";
 import PropTypes from 'prop-types';
-import {reproject} from 'reproject';
-import epsg from 'epsg'
 import * as turf from '@turf/turf';
 import SubProjectPopupDetail from "./SubProjectPopup";
 
-function SubProjectPoints({subProjects, getSubproject, subProjectLoading, subProject, project}) {
+function SubProjectPoints({ subProjects, getSubproject, subProjectLoading, subProject, project }) {
     const [zoomLevel, setZoomLevel] = useState(0);
 
     const map = useMapEvents({
@@ -22,16 +20,16 @@ function SubProjectPoints({subProjects, getSubproject, subProjectLoading, subPro
     return (
         <>
             {subProjects.map((subProject) => {
-                const {name, id} = subProject;
+                const { name, id } = subProject;
                 let polygon, geo_json;
-                if (subProject?.geo_json && subProject?.geo_json?.srid) {
-                    geo_json = subProject.geo_json?.srid === 'EPSG:4326' ? subProject.geo_json : reproject(subProject.geo_json, subProject.geo_json?.srid, 'EPSG:4326', epsg);
+                if (subProject?.geo_json) {
+                    geo_json = subProject.geo_json;
                     polygon = geo_json.geometry;
                 } else {
                     polygon = JSON.parse(subProject.district.geom);
                     geo_json = JSON.parse(subProject.district.geom);
                 }
-                const {geometry} = turf.pointOnFeature(polygon);
+                const { geometry } = turf.pointOnFeature(polygon);
                 const renderMarker = () => (<Marker
                     position={[geometry.coordinates[1], geometry.coordinates[0]]}
                     title={name}
@@ -45,21 +43,19 @@ function SubProjectPoints({subProjects, getSubproject, subProjectLoading, subPro
 
                 const renderPolygon = () => (<GeoJSON
                     key={`${id}-polygon`}
-                    style={{weight: 10}}
+                    style={{ weight: 10 }}
                     data={geo_json}
-                    eventHandlers={{click: () => handlePopup(id)}}
+                    eventHandlers={{ click: () => handlePopup(id) }}
                 >
                     <Popup>
                         <SubProjectPopupDetail subProject={subProject} project={project}
-                                               subProjectLoading={subProjectLoading}/>
+                            subProjectLoading={subProjectLoading} />
                     </Popup>
                 </GeoJSON>);
 
                 return (
                     <div>
-                        {
-                            subProject?.geo_json ? zoomLevel < 12 ? renderMarker() : renderPolygon() : renderMarker()
-                        }
+                        {subProject?.geo_json ? zoomLevel < 12 ? renderMarker() : renderPolygon() : renderMarker()}
                     </div>
                 );
 
